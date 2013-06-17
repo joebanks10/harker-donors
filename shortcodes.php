93f06488f257b05bcbb36f2ae12c637489645fab
Joe Banks <joeb@harker.org>
2013-06-17 14:11:57 -0700

    Changed last modified date

--- shortcodes.php
+++ shortcodes.php
@@ -2850,7 +2850,7 @@ function hkr_dnrs_get_title_by_org( $cons_custom ) {
 add_filter( 'hkr_dnrs_list', 'hkr_dnrs_print_last_modified', 1 );
 
 function hkr_dnrs_print_last_modified( $content ) {
-    return $content . '<p><b>Last updated:</b> May 23, 2013</p>'; // TODO: make dynamic!
+    return $content . '<p><b>Last updated:</b> May 31, 2013</p>'; // TODO: make dynamic!
 }
 
 
