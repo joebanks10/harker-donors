0d7a98c9e38fae6dd3a170f1207f9552b0b57e1f
Joe Banks <joeb@harker.org>
2017-01-04 11:31:05 -0800

    Fix touch events for d3 tip.

--- js/giving-by-class-chart.js
+++ js/giving-by-class-chart.js
@@ -22,6 +22,7 @@ var BarChart = (function($) {
     var tip = d3.tip().attr('class', 'd3-tip n').offset([-40, 0]).html(function(d) {
       return 'Class of ' + d.class + ': ' + Math.round(d.gave_percent * 100) + '% (' + d.gave_count + '/' + d.student_count + ')';
     });
+    var isShowingTip = -1;
 
     var x = d3.scaleBand().rangeRound([0, width]).padding(0.1),
         y = d3.scaleLinear().rangeRound([height, 0]);
@@ -79,8 +80,15 @@ var BarChart = (function($) {
           .attr("height", 0)
           .on('mouseover', tip.show)
           .on('mouseout', tip.hide)
-          .on('touchstart', tip.show)
-          .on('touchend', tip.hide)
+          .on('touchstart', function(d, i) {
+            if (isShowingTip === i) {
+              tip.hide(d);
+              isShowingTip = -1;
+            } else {
+              tip.show(d);
+              isShowingTip = i;
+            }
+          })
           .transition()
           .duration(2000)
           .delay(function(d, i) { return 250 + i*50; })
